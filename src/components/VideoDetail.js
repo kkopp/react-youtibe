fcf3ac682d90c8c46441e486ae17645a7c5370b5
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-02-02 09:40:38 -0700

    start of react youtube project

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+    if (!video) {
+        return <p>Loading spinner goes here...</p>
+    }
+
+    console.log(video);
+    const videoId =  video.id.videoId;
+    const videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+
+
+    return (
+        <>
+        <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src={videoUrl} allowFullScreen></iframe>
+        </div>
+        <h2>{video.snippet.title}</h2>
+        <p>{video.snippet.description}</p>
+        </>
+    )
+};
+
+export default VideoDetail;
---
+++ src/components/VideoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const VideoList = props => {
+    return (
+        <ul>
+            {props.children}
+        </ul>
+    )
+}
+
+export const VideoListItem = () => {
+    return (
+        <li>
+            Video List Item
+        </li>
+    )
+}
